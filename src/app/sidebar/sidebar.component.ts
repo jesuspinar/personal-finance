8e2fb516463c7d3afe2fb0338b7687cb07a0c5e0
jesuspinar <devjesuspinar@gmail.com>
2025-11-05 18:14:18 +0000

    Fix link active

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  imports: [RouterLink]
+  imports: [RouterLink, RouterLinkActive]
 })
 export class SidebarComponent {}
