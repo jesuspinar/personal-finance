2ea2c70c9a1e0bb95097198617746b870b23c93a
jesuspinar <devjesuspinar@gmail.com>
2025-11-06 21:17:32 +0000

    Add edit income with persistence

--- src/app/income-form/income-form.component.ts
+++ src/app/income-form/income-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { OfflineDbService } from '../offline-db.service';
 
@@ -12,9 +12,11 @@ import { OfflineDbService } from '../offline-db.service';
 })
 export class IncomeFormComponent implements OnInit {
   private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
   private readonly fb = inject(FormBuilder);
   private readonly db = inject(OfflineDbService);
   mode = signal<'add' | 'edit'>('add');
+  currentId: number | null = null;
 
   form = this.fb.group({
     description: ['', [Validators.required]],
@@ -23,16 +25,35 @@ export class IncomeFormComponent implements OnInit {
   });
 
   ngOnInit(): void {
-    if (this.router.url.includes('add-income')) {
-      this.mode.set('add');
-    } else {
-      this.mode.set('edit');
-    }
+    this.route.queryParams.subscribe(params => {
+      const idParam = params['id'];
+      if (idParam) {
+        this.mode.set('edit');
+        this.currentId = +idParam;
+        this.db.incomes.get(this.currentId).then(income => {
+          if (income) {
+            this.form.patchValue({
+              description: income.description,
+              amount: income.amount.toString(),
+              date: income.date
+            });
+          }
+        });
+      } else {
+        this.mode.set('add');
+      }
+    });
   }
 
   onSubmit(): void {
     if (this.form.valid) {
-      this.db.incomes.add(this.form.value as any).then(() => this.router.navigate(['/income']));
+      if (this.mode() === 'edit' && this.currentId != null) {
+        this.db.incomes.update(this.currentId, this.form.value as any)
+          .then(() => this.router.navigate(['/income']));
+      } else {
+        this.db.incomes.add(this.form.value as any)
+          .then(() => this.router.navigate(['/income']));
+      }
     }
   }
 }
